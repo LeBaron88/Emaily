4c994d84db824208b787e4f4469ab3adf497e3b6
Ruth Mutanga <simon.mpoyus@gmail.com>
2019-09-25 20:15:54 -0400

    push build

---
+++ client/build/precache-manifest.79e73c5c8d44fa30a79dd53c04bc5ab6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13b6503440da62747bd7a27c66d5f009",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d561f6828d21d0a68385",
+    "url": "/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "d561f6828d21d0a68385",
+    "url": "/static/js/2.ed173158.chunk.js"
+  },
+  {
+    "revision": "31fc3c3ffacd49972e37",
+    "url": "/static/js/main.37921a9a.chunk.js"
+  },
+  {
+    "revision": "2055ac218fe3f7ad47ca",
+    "url": "/static/js/runtime-main.a7ca617d.js"
+  }
+]);
